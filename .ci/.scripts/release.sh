1c91637df900e917438083f7e7e52ab792793885
Auronen <HThiele@seznam.cz>
2025-01-05 16:02:03 +0100

    tbs

--- .ci/.scripts/release.sh
+++ .ci/.scripts/release.sh
@@ -40,6 +40,7 @@ echo "Substituting Gothic 2 GMM..."
 mkdir -p release/{langs,release}
 cp -R .ci/.release/* release/release
 
+cd ..
 ls -la
 
 .ci/.scripts/dacode subs -i cp1252 -c "_work/Data/Scripts" -b "g2loc/.translations" -t ".translations" -d "release/langs"
